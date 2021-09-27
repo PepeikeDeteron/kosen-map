c0b55315e4bd9143127425d58a2c13310ad9ad73
deteikepeperon@gmail.com <deteikepeperon@gmail.com>
2021-09-27 12:38:21 +0900

    styled-components を導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Head from 'next/head'
 import { AppProps } from 'next/app'
+import 'normalize.css'
+import '../styles/globals.css'
 
 const App: React.VFC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
---
+++ src/pages/_document.tsx
@@ -0,0 +1,51 @@
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+
+class CustomDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="ja">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default CustomDocument
+
+CustomDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const sheet = new ServerStyleSheet()
+  const originalRenderPage = ctx.renderPage
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      })
+
+    const initialProps = await Document.getInitialProps(ctx)
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      ),
+    }
+  } finally {
+    sheet.seal()
+  }
+}
