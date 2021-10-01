208478149fe34635cc8c2ff6ba24c8bae42f1a67
Yoshiaki SASAKI <deteikepeperon@gmail.com>
2021-10-01 21:48:45 +0900

    Add: MUI (v5) を導入 (#2)

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/system'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#226cd6',
+    },
+    secondary: {
+      main: '#3cb371',
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+})
+
+export default theme
