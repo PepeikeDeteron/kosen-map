ff299b655ac887cb33618d03098a0ed0dcdb1fb3
deteikepeperon@gmail.com <deteikepeperon@gmail.com>
2021-10-03 11:13:22 +0900

    MapDisplay コンポーネントを追加

---
+++ src/components/molecules/MapDisplay/index.stories.tsx
@@ -0,0 +1,22 @@
+import React, { ComponentProps } from 'react'
+import { Meta, Story } from '@storybook/react'
+import MapDisplay from '.'
+
+export default {
+  title: 'components/molecules/MapDisplay',
+  components: MapDisplay,
+  argTypes: {
+    onClick: {
+      action: 'clicked',
+    },
+  },
+} as Meta
+
+const Template: Story<ComponentProps<typeof MapDisplay>> = (props) => (
+  <MapDisplay {...props} />
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  children: 'ここに地図を表示',
+}
