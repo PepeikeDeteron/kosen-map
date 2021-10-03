688669b9133a8a82d67e6aebcdc7b498e017bd50
deteikepeperon@gmail.com <deteikepeperon@gmail.com>
2021-10-03 12:09:25 +0900

    HomeButton コンポーネントを追加

---
+++ src/components/molecules/HomeButton/index.stories.tsx
@@ -0,0 +1,20 @@
+import React, { ComponentProps } from 'react'
+import { Meta, Story } from '@storybook/react'
+import HomeButton from '.'
+
+export default {
+  title: 'components/molecules/HomeButton',
+  components: HomeButton,
+  argTypes: {
+    onClick: {
+      action: 'clicked',
+    },
+  },
+} as Meta
+
+const Template: Story<ComponentProps<typeof HomeButton>> = (props) => (
+  <HomeButton {...props} />
+)
+
+export const Default = Template.bind({})
+Default.args = {}
