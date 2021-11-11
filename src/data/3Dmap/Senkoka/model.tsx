3e45cc07f20e99092bddbb9fe159127a4d323ff8
Yoshiaki SASAKI <pepeikedeteron@gmail.com>
2021-11-11 10:48:41 +0900

    Refactor: Three.js のパフォーマンス向上を試みる (#35)
    * 不要な Geometry, Material, Mesh を削除
    
    * PerspectiveCamera の near と far が極力 scene ギリギリを維持するように変更
    
    * ロード中の解析とロード後のコールバック処理との間に setTimeOut を仕込む

--- src/data/3Dmap/Senkoka/model.tsx
+++ src/data/3Dmap/Senkoka/model.tsx
@@ -11,66 +11,66 @@ const Model: React.VFC = () => {
       alpha: true,
     })
     const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(45, 16 / 10, 1, 100000)
+    const camera = new THREE.PerspectiveCamera(45, 16 / 10, 1, 18475)
     const light = new THREE.AmbientLight(0x666666, 2.5)
     const orbitControls = new OrbitControls(camera, renderer.domElement)
-
-    const geometry = new THREE.SphereGeometry(1)
-    const material = new THREE.MeshNormalMaterial()
-    const mesh = new THREE.Mesh(geometry, material)
+    const mtlLoader = new MTLLoader()
 
     camera.position.set(0, 0, 15000)
     orbitControls.enableDamping = true
     orbitControls.dampingFactor = 0.2
-    scene.add(mesh)
     scene.add(light)
-
-    const mtlLoader = new MTLLoader()
-    const objLoader = new OBJLoader()
 
     mtlLoader.setPath('senkoka/')
     mtlLoader.load('senkoka.mtl', (material) => {
-      material.preload()
+      const mtlLoadEnd = () => {
+        const objLoader = new OBJLoader()
 
-      objLoader.setMaterials(material)
-      objLoader.setPath('senkoka/')
-      objLoader.load(
-        'senkoka.obj',
-        // 読込処理
-        (object) => {
-          const objects: THREE.Group[] = []
+        material.preload()
+        objLoader.setMaterials(material)
+        objLoader.setPath('senkoka/')
+        objLoader.load(
+          // 読込処理
+          'senkoka.obj',
+          (object) => {
+            const objLoadEnd = () => {
+              const objects: THREE.Group[] = []
 
-          object.scale.set(130, 180, 150)
-          object.rotation.set(0, -100, 0)
-          object.position.set(-300, -4000, 0)
+              object.scale.set(130, 180, 150)
+              object.rotation.set(0, -100, 0)
+              object.position.set(-300, -4000, 0)
 
-          scene.add(object)
-          objects.push(object)
-        },
-        // 読込状況
-        (xhr) => {
-          const loader = document.getElementById('loader') as HTMLElement
-          const progress = Math.ceil((xhr.loaded / xhr.total) * 100)
+              scene.add(object)
+              objects.push(object)
+            }
 
-          if (progress < 100) {
-            loader.innerHTML = `Loading... ${progress}%`
-          } else {
-            setInterval(() => {
-              loader.style.display = 'none'
-            }, 1000)
+            setTimeout(objLoadEnd, 10)
+          },
+          // 読込状況
+          (xhr) => {
+            const loader = document.getElementById('loader') as HTMLElement
+            const progress = Math.ceil((xhr.loaded / xhr.total) * 100)
+
+            if (progress < 100) {
+              loader.innerHTML = `Loading... ${progress}%`
+            } else {
+              setInterval(() => {
+                loader.style.display = 'none'
+              }, 1000)
+            }
+          },
+          // 読込失敗
+          (error) => {
+            console.error(`Three.js: ${error}`)
           }
-        },
-        // 読込失敗
-        (error) => {
-          console.error(`Three.js: ${error}`)
-        }
-      )
+        )
+      }
+
+      setTimeout(mtlLoadEnd, 10)
     })
 
     const tick = () => {
-      mesh.rotation.y += 0.01
       renderer.render(scene, camera)
-
       requestAnimationFrame(tick)
     }
 
