781cfbb9b451bb411a9224d565fb46d5e2ca019e
Yoshiaki SASAKI <pepeikedeteron@gmail.com>
2021-12-13 17:08:51 +0900

    glb データに変換、読込

--- src/data/3Dmap/Senkoka/index.tsx
+++ src/data/3Dmap/Senkoka/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import * as THREE from 'three'
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const Model: React.VFC = () => {
@@ -12,62 +11,40 @@ const Model: React.VFC = () => {
     })
     const scene = new THREE.Scene()
     const camera = new THREE.PerspectiveCamera(45, 16 / 10, 1, 1000000)
-    const light = new THREE.AmbientLight(0x666666, 2.5)
-    const orbitControls = new OrbitControls(camera, renderer.domElement)
-    const mtlLoader = new MTLLoader()
+    const light = new THREE.AmbientLight(0xffffff, 1.75)
+    const controls = new OrbitControls(camera, renderer.domElement)
 
     camera.position.set(0, 0, 15000)
-    orbitControls.enableDamping = true
-    orbitControls.dampingFactor = 0.2
+    controls.enableDamping = true
+    controls.dampingFactor = 0.2
     scene.add(light)
 
-    mtlLoader.setPath('senkoka/')
-    mtlLoader.load('senkoka.mtl', (material) => {
-      const mtlLoadEnd = () => {
-        const objLoader = new OBJLoader()
+    const loader = new GLTFLoader()
 
-        material.preload()
-        objLoader.setMaterials(material)
-        objLoader.setPath('senkoka/')
-        objLoader.load(
-          // 読込処理
-          'senkoka.obj',
-          (object) => {
-            const objLoadEnd = () => {
-              const objects: THREE.Group[] = []
+    loader.load(
+      'senkoka.glb',
+      (gltf) => {
+        const model = gltf.scene
 
-              object.scale.set(130, 180, 150)
-              object.rotation.set(0, -100, 0)
-              object.position.set(-300, -4000, 0)
+        model.scale.set(130, 180, 150)
+        model.rotation.set(0, -100, 0)
+        model.position.set(-300, -4000, 0)
 
-              scene.add(object)
-              objects.push(object)
-            }
+        scene.add(model)
+      },
+      // 読込状況
+      (xhr) => {
+        const progress = Math.ceil((xhr.loaded / xhr.total) * 100)
 
-            setTimeout(objLoadEnd, 10)
-          },
-          // 読込状況
-          (xhr) => {
-            const loader = document.getElementById('loader') as HTMLElement
-            const progress = Math.ceil((xhr.loaded / xhr.total) * 100)
-
-            if (progress < 100) {
-              loader.innerHTML = `Loading... ${progress}%`
-            } else {
-              setInterval(() => {
-                loader.style.display = 'none'
-              }, 1000)
-            }
-          },
-          // 読込失敗
-          (error) => {
-            console.error(`Three.js: ${error}`)
-          }
-        )
+        if (progress < 100) {
+          console.log(`nitic-map: Loading... ${progress}%`)
+        }
+      },
+      // 読込失敗
+      (error) => {
+        console.error(`nitic-map: ${error}`)
       }
-
-      setTimeout(mtlLoadEnd, 10)
-    })
+    )
 
     const tick = () => {
       renderer.render(scene, camera)
@@ -99,7 +76,6 @@ const Model: React.VFC = () => {
 
   return (
     <>
-      <p id="loader">Loading... 0%</p>
       <canvas id="canvas" />
     </>
   )
