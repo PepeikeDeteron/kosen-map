f5c0b5923886f5255ecf8658c8fbe60f3ee77d32
Yoshiaki SASAKI <pepeikedeteron@gmail.com>
2022-01-01 20:59:43 +0900

    Update: templates ディレクトリの Storybook を更新 (#88)
    * 専攻科表示画面の Storybook を追加
    
    * 教育棟表示画面の Storybook を修正
    
    * スナップショットを更新

--- src/components/templates/Senkoka/index.stories.tsx
+++ src/components/templates/Senkoka/index.stories.tsx
@@ -1,15 +1,15 @@
 import React, { ComponentProps } from 'react';
 import { Meta, Story } from '@storybook/react';
-import Kyoiku from '.';
+import Senkoka from '.';
 
 export default {
-  title: 'components/templates/Kyoiku',
-  components: Kyoiku,
+  title: 'components/templates/Senkoka',
+  components: Senkoka,
   argTypes: {},
 } as Meta;
 
-const Template: Story<ComponentProps<typeof Kyoiku>> = (props) => (
-  <Kyoiku {...props} />
+const Template: Story<ComponentProps<typeof Senkoka>> = (props) => (
+  <Senkoka {...props} />
 );
 
 export const Default = Template.bind({});
