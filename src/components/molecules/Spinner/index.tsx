f374f5089da68516dacb46e392c68ccbaf049a26
Yoshiaki SASAKI <pepeikedeteron@gmail.com>
2022-01-03 19:40:14 +0900

    Style: ローディング画面の文字サイズを修正 (#106)
    * Spinner の文字サイズを変更
    
    * スナップショットを更新
    
    * フォントの行間を修正
    
    * スナップショットを更新

--- src/components/molecules/Spinner/index.tsx
+++ src/components/molecules/Spinner/index.tsx
@@ -30,11 +30,11 @@ const StyledComponent = styled(Component)`
   left: 50%;
   bottom: 0;
   color: gray;
-  font-size: 2rem;
+  font-size: 1.5rem;
   text-align: center;
   height: 25vh;
   transform: translateX(-50%);
-  line-height: 30px;
+  line-height: 25px;
 `;
 
 const Container: React.VFC<ContainerProps> = (props) => {
