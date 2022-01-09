06333f0432a93529d36e06bb28b52b40c55dc81d
Yoshiaki SASAKI <pepeikedeteron@gmail.com>
2022-01-09 17:47:55 +0900

    教員室と1年ごとに部屋移動する教室名だけ環境変数に登録

---
+++ src/data/room.ts
@@ -0,0 +1,267 @@
+// 専攻科・教育棟 1階案内
+export const SENKOKA_101 = '講義室1';
+export const SENKOKA_102 = 'コンピューター室';
+export const SENKOKA_103 = '生産工学実験室';
+export const SENKOKA_104 = '物質化学工学実験室';
+export const SENKOKA_105 = '機器測定室';
+export const SENKOKA_106 = '講義室2';
+
+// 専攻科・教育棟 2階案内
+export const SENKOKA_201 = 'ゼミ室1';
+export const SENKOKA_202 = 'ゼミ室2';
+export const SENKOKA_203 = `${process.env.SENKOKA_203}`;
+export const SENKOKA_204 = `${process.env.SENKOKA_204}`;
+export const SENKOKA_205 = 'ゼミ室3';
+
+// 専攻科・教育棟 3階案内
+export const SENKOKA_301 = `${process.env.SENKOKA_301}`;
+export const SENKOKA_302 = `${process.env.SENKOKA_302}`;
+export const SENKOKA_303 = `${process.env.SENKOKA_303}`;
+export const SENKOKA_304 = `${process.env.SENKOKA_304}`;
+
+// 専攻科・教育棟 4階案内
+export const SENKOKA_401 = `${process.env.SENKOKA_401}`;
+export const SENKOKA_402 = `${process.env.SENKOKA_402}`;
+export const SENKOKA_403 = `${process.env.SENKOKA_403}`;
+export const SENKOKA_404 = `${process.env.SENKOKA_404}`;
+
+// 専攻科・教育棟 5階案内
+export const SENKOKA_501 = `${process.env.SENKOKA_501}`;
+export const SENKOKA_502 = `${process.env.SENKOKA_502}`;
+export const SENKOKA_503 = `${process.env.SENKOKA_503}`;
+export const SENKOKA_504 = `${process.env.SENKOKA_504}`;
+
+// 管理・教育棟＆1~7号棟 総合案内
+export const KYOIKU_0000 = '管理・教育棟';
+export const KYOIKU_1000 = '1号棟';
+export const KYOIKU_2000 = '2号棟';
+export const KYOIKU_3000 = '3号棟';
+export const KYOIKU_4000 = '4号棟';
+export const KYOIKU_5000 = '5号棟';
+export const KYOIKU_6000 = '6号棟';
+export const KYOIKU_7000 = '7号棟';
+
+// 管理・教育棟 1階案内
+export const KYOIKU_0101 = '倉庫1';
+export const KYOIKU_0102 = '倉庫2';
+export const KYOIKU_0103 = '総務課・財務課';
+export const KYOIKU_0104 = 'トレーナー室';
+export const KYOIKU_0105 = '第一実習室';
+export const KYOIKU_0106 = '第二実習室';
+export const KYOIKU_0107 = 'サーバー室';
+export const KYOIKU_0108 = '男子ロッカー室';
+export const KYOIKU_0109 = '女子ロッカー室';
+export const KYOIKU_0110 = '給湯室';
+export const KYOIKU_0111 = '第一講義室';
+export const KYOIKU_0112 = '共通資料室';
+export const KYOIKU_0113 = 'メールボックス室';
+export const KYOIKU_0114 = '共通会議室';
+export const KYOIKU_0115 = `${process.env.KYOIKU_0115}`;
+export const KYOIKU_0116 = '技術室';
+export const KYOIKU_0117 = '工作室';
+export const KYOIKU_0118 = '創造工房';
+export const KYOIKU_0119 = `${process.env.KYOIKU_0119}`;
+export const KYOIKU_0120 = `${process.env.KYOIKU_0120}`;
+
+// 管理・教育棟 2階案内
+export const KYOIKU_0201 = '進路相談室';
+export const KYOIKU_0202 = '特別会議室';
+export const KYOIKU_0203 = '事務部長室';
+export const KYOIKU_0204 = '校長室';
+export const KYOIKU_0205 = '総務課・学生課';
+export const KYOIKU_0206 = '進路情報コーナー';
+export const KYOIKU_0207 = '学生会室';
+export const KYOIKU_0208 = `${process.env.KYOIKU_0208}`;
+export const KYOIKU_0209 = `${process.env.KYOIKU_0209}`;
+
+// 管理・教育棟 3階案内
+export const KYOIKU_0301 = `${process.env.KYOIKU_0301}`;
+export const KYOIKU_0302 = `${process.env.KYOIKU_0302}`;
+export const KYOIKU_0303 = `${process.env.KYOIKU_0303}`;
+export const KYOIKU_0304 = '資料室 (管-304)';
+export const KYOIKU_0305 = `${process.env.KYOIKU_0305}`;
+export const KYOIKU_0306 = `${process.env.KYOIKU_0306}`;
+export const KYOIKU_0307 = '資料室 (管-307)';
+export const KYOIKU_0308 = `${process.env.KYOIKU_0308}`;
+export const KYOIKU_0309 = '資料室・多目的ｽﾍﾟｰｽ';
+export const KYOIKU_0310 = `${process.env.KYOIKU_0310}`;
+export const KYOIKU_0311 = `${process.env.KYOIKU_0311}`;
+export const KYOIKU_0312 = `${process.env.KYOIKU_0312}`;
+export const KYOIKU_0313 = `${process.env.KYOIKU_0313}`;
+export const KYOIKU_0314 = `${process.env.KYOIKU_0314}`;
+export const KYOIKU_0315 = `${process.env.KYOIKU_0315}`;
+export const KYOIKU_0316 = `${process.env.KYOIKU_0316}`;
+
+// 1号棟 1階案内
+export const KYOIKU_1101 = '共通印刷室';
+export const KYOIKU_1102 = '機械工学科室';
+export const KYOIKU_1103 = '研究室 1-103';
+export const KYOIKU_1104 = `${process.env.KYOIKU_1104}`;
+export const KYOIKU_1105 = `${process.env.KYOIKU_1105}`;
+export const KYOIKU_1106 = `${process.env.KYOIKU_1106}`;
+export const KYOIKU_1107 = '材料強度実験室';
+export const KYOIKU_1108 = '流体工学実験室';
+export const KYOIKU_1109 = '流体工学計算室';
+export const KYOIKU_1110 = '自動車ﾘｽｸ工学実験室';
+export const KYOIKU_1111 = '人間医工学実験室';
+
+// 1号棟 2階案内
+export const KYOIKU_1201 = '共通非常勤講師室';
+export const KYOIKU_1202 = `${process.env.KYOIKU_1202}`;
+export const KYOIKU_1203 = `${process.env.KYOIKU_1203}`;
+export const KYOIKU_1204 = `${process.env.KYOIKU_1204}`;
+export const KYOIKU_1205 = '制御情報工学科室';
+export const KYOIKU_1206 = 'メカトロニクス室';
+export const KYOIKU_1207 = 'センサ工学実験室';
+export const KYOIKU_1208 = '設計製図室';
+export const KYOIKU_1209 = '精密測定実験室';
+
+// 1号棟 3階案内
+export const KYOIKU_1301 = `${process.env.KYOIKU_1301}`;
+export const KYOIKU_1302 = `${process.env.KYOIKU_1302}`;
+export const KYOIKU_1303 = `${process.env.KYOIKU_1303}`;
+export const KYOIKU_1304 = `${process.env.KYOIKU_1304}`;
+export const KYOIKU_1305 = `${process.env.KYOIKU_1305}`;
+export const KYOIKU_1306 = `${process.env.KYOIKU_1306}`;
+export const KYOIKU_1307 = `${process.env.KYOIKU_1307}`;
+export const KYOIKU_1308 = `${process.env.KYOIKU_1308}`;
+export const KYOIKU_1309 = `${process.env.KYOIKU_1309}`;
+export const KYOIKU_1310 = `${process.env.KYOIKU_1310}`;
+export const KYOIKU_1311 = `${process.env.KYOIKU_1311}`;
+export const KYOIKU_1312 = '情報演習室';
+export const KYOIKU_1313 = '工学デザイン室';
+
+// 2号棟 1階案内
+export const KYOIKU_2101 = '変電室';
+export const KYOIKU_2102 = '研究室 2-102';
+export const KYOIKU_2103 = `${process.env.KYOIKU_2103}`;
+export const KYOIKU_2104 = '一般化学実験室';
+export const KYOIKU_2105 = '準備室';
+export const KYOIKU_2106 = `${process.env.KYOIKU_2106}`;
+export const KYOIKU_2107 = '研究室 2-107';
+export const KYOIKU_2108 = '計測室1';
+export const KYOIKU_2109 = '女子更衣室';
+
+// 2号棟 2階案内
+export const KYOIKU_2201 = 'コミュニケーションルーム';
+export const KYOIKU_2202 = '研究室 2-202';
+export const KYOIKU_2203 = `${process.env.KYOIKU_2203}`;
+export const KYOIKU_2204 = '物理化学実験室';
+export const KYOIKU_2205 = '準備室';
+export const KYOIKU_2206 = `${process.env.KYOIKU_2206}`;
+export const KYOIKU_2207 = '研究室 2-207';
+export const KYOIKU_2208 = '化学第一機器分析室';
+
+// 2号棟 3階案内
+export const KYOIKU_2301 = `${process.env.KYOIKU_2301}`;
+export const KYOIKU_2302 = '研究室 2-302';
+export const KYOIKU_2303 = '研究室 2-303';
+export const KYOIKU_2304 = `${process.env.KYOIKU_2304}`;
+export const KYOIKU_2305 = '分析化学実験室';
+export const KYOIKU_2306 = '倉庫';
+export const KYOIKU_2307 = '天秤室';
+export const KYOIKU_2308 = '化学共同実験室';
+export const KYOIKU_2309 = '準備室';
+
+// 2号棟 4階案内
+export const KYOIKU_2401 = `${process.env.KYOIKU_2401}`;
+export const KYOIKU_2402 = '工業化学小実験室';
+export const KYOIKU_2403 = `${process.env.KYOIKU_2403}`;
+export const KYOIKU_2404 = '工業化学実験室';
+export const KYOIKU_2405 = '準備室';
+export const KYOIKU_2406 = `${process.env.KYOIKU_2406}`;
+export const KYOIKU_2407 = '有機化学共同実験室';
+export const KYOIKU_2408 = '計測室2';
+
+// 3号棟 1階案内
+export const KYOIKU_3101 = 'ｴﾈﾙｷﾞｰ制御実験室';
+export const KYOIKU_3102 = 'ｴﾈﾙｷﾞｰ応用実験室';
+export const KYOIKU_3103 = 'ｴﾈﾙｷﾞｰ演習室';
+export const KYOIKU_3104 = 'ｴﾈﾙｷﾞｰ基礎実験室';
+
+// 3号棟 2階案内
+export const KYOIKU_3201 = '材料工学実験室';
+export const KYOIKU_3202 = '準備室';
+export const KYOIKU_3203 = `${process.env.KYOIKU_3203}`;
+
+// 4号棟 1階案内
+export const KYOIKU_4101 = '光学実験室';
+export const KYOIKU_4102 = '電気精密測定室';
+export const KYOIKU_4103 = '研究室 4-103';
+export const KYOIKU_4104 = '研究室 4-104';
+export const KYOIKU_4105 = '高電圧実験室';
+export const KYOIKU_4106 = '電気機械実験室';
+
+// 4号棟 2階案内
+export const KYOIKU_4201 = '電気情報工学科室';
+export const KYOIKU_4202 = `${process.env.KYOIKU_4202}`;
+export const KYOIKU_4203 = `${process.env.KYOIKU_4203}`;
+export const KYOIKU_4204 = `${process.env.KYOIKU_4204}`;
+export const KYOIKU_4205 = `${process.env.KYOIKU_4205}`;
+export const KYOIKU_4206 = `${process.env.KYOIKU_4206}`;
+export const KYOIKU_4207 = '電気情報基礎実験室';
+
+// 4号棟 3階案内
+export const KYOIKU_4301 = `${process.env.KYOIKU_4301}`;
+export const KYOIKU_4302 = `${process.env.KYOIKU_4302}`;
+export const KYOIKU_4303 = `${process.env.KYOIKU_4303}`;
+export const KYOIKU_4304 = `${process.env.KYOIKU_4304}`;
+export const KYOIKU_4305 = `${process.env.KYOIKU_4305}`;
+export const KYOIKU_4306 = `${process.env.KYOIKU_4306}`;
+export const KYOIKU_4307 = `${process.env.KYOIKU_4307}`;
+export const KYOIKU_4308 = '電気情報応用実験室';
+
+// 4号棟 4階案内
+export const KYOIKU_4401 = '研究室 4-401';
+export const KYOIKU_4402 = '研究室 4-402';
+export const KYOIKU_4403 = '研究室 4-403';
+export const KYOIKU_4404 = '研究室 4-404';
+export const KYOIKU_4405 = '研究室 4-405';
+export const KYOIKU_4406 = '電気情報通信実験室';
+
+// 5号棟 1階案内
+export const KYOIKU_5101 = '制御応用・電子工学実験室';
+export const KYOIKU_5102 = `${process.env.KYOIKU_5102}`;
+export const KYOIKU_5103 = `${process.env.KYOIKU_5103}`;
+export const KYOIKU_5104 = `${process.env.KYOIKU_5104}`;
+export const KYOIKU_5105 = '物質化学研究室';
+export const KYOIKU_5106 = '化学・バイオ系共同実験室';
+export const KYOIKU_5107 = '研究室 5-107';
+
+// 5号棟 2階案内
+export const KYOIKU_5201 = `${process.env.KYOIKU_5201}`;
+export const KYOIKU_5202 = `${process.env.KYOIKU_5202}`;
+export const KYOIKU_5203 = `${process.env.KYOIKU_5203}`;
+export const KYOIKU_5204 = `${process.env.KYOIKU_5204}`;
+export const KYOIKU_5205 = `${process.env.KYOIKU_5205}`;
+export const KYOIKU_5206 = `${process.env.KYOIKU_5206}`;
+export const KYOIKU_5207 = '情報ｾｷｭﾘﾃｨ演習室';
+
+// 5号棟 3階案内
+export const KYOIKU_5301 = '物理実験室';
+export const KYOIKU_5302 = `${process.env.KYOIKU_5302}`;
+export const KYOIKU_5303 = '研究室 5-303';
+export const KYOIKU_5304 = '研究室 5-304';
+
+// 6号棟 1階案内
+export const KYOIKU_6101 = `${process.env.KYOIKU_6101}`;
+export const KYOIKU_6102 = '第二機器分析室';
+export const KYOIKU_6103 = 'プロセス工学実験室';
+export const KYOIKU_6104 = '機材室';
+
+// 6号棟 2階案内
+export const KYOIKU_6201 = `${process.env.KYOIKU_6201}`;
+export const KYOIKU_6202 = '生物工学実験室3';
+export const KYOIKU_6203 = '生物工学実験室2';
+export const KYOIKU_6204 = '生物工学実験室1';
+export const KYOIKU_6205 = '薬品室';
+
+// 7号棟 1階案内
+export const KYOIKU_7101 = `${process.env.KYOIKU_7101}`;
+export const KYOIKU_7102 = '制御工学・電子回路実験室';
+export const KYOIKU_7103 = '加工システム実験室';
+
+// 7号棟 2階案内
+export const KYOIKU_7201 = `${process.env.KYOIKU_7201}`;
+export const KYOIKU_7202 = 'コンピューター支援設計演習室';
+export const KYOIKU_7203 = 'マイコン応用演習室 電子応用実験室';
