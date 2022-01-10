f09e9aa6285be85144439755a2179fbaed25d5e0
Yoshiaki SASAKI <pepeikedeteron@gmail.com>
2022-01-10 15:55:11 +0900

    ガイド情報を各系ごとに分ける

--- src/data/kyoiku.ts
+++ src/data/kyoiku.ts
@@ -380,6 +380,7 @@ import {
 } from '@/libs/Three/KyoikuDivide';
 import { GuideProps } from '@/models/guide';
 
+// 管理・教育棟全体図
 export const kyoiku: GuideProps[] = [
   {
     id: 0,
@@ -423,7 +424,671 @@ export const kyoiku: GuideProps[] = [
   },
 ];
 
-export const kyoikuDivide: GuideProps[] = [
+// 共通施設
+export const kyoikuCommonFacilities: GuideProps[] = [
+  {
+    id: 205,
+    name: KYOIKU_0205,
+    position: kyoiku0205,
+  },
+  {
+    id: 103,
+    name: KYOIKU_0103,
+    position: kyoiku0103,
+  },
+  {
+    id: 111,
+    name: KYOIKU_0111,
+    position: kyoiku0111,
+  },
+  {
+    id: 105,
+    name: KYOIKU_0105,
+    position: kyoiku0105,
+  },
+  {
+    id: 106,
+    name: KYOIKU_0106,
+    position: kyoiku0106,
+  },
+  {
+    id: 104,
+    name: KYOIKU_0104,
+    position: kyoiku0104,
+  },
+  {
+    id: 106,
+    name: KYOIKU_0106,
+    position: kyoiku0106,
+  },
+  {
+    id: 114,
+    name: KYOIKU_0114,
+    position: kyoiku0114,
+  },
+  {
+    id: 201,
+    name: KYOIKU_0201,
+    position: kyoiku0201,
+  },
+  {
+    id: 202,
+    name: KYOIKU_0202,
+    position: kyoiku0202,
+  },
+  {
+    id: 203,
+    name: KYOIKU_0203,
+    position: kyoiku0203,
+  },
+  {
+    id: 204,
+    name: KYOIKU_0204,
+    position: kyoiku0204,
+  },
+  {
+    id: 206,
+    name: KYOIKU_0206,
+    position: kyoiku0206,
+  },
+  {
+    id: 207,
+    name: KYOIKU_0207,
+    position: kyoiku0207,
+  },
+  {
+    id: 112,
+    name: KYOIKU_0112,
+    position: kyoiku0112,
+  },
+  {
+    id: 113,
+    name: KYOIKU_0113,
+    position: kyoiku0113,
+  },
+];
+
+// 機械・知能系
+export const kyoikuMachineGroup: GuideProps[] = [
+  {
+    id: 314,
+    name: KYOIKU_0314,
+    position: kyoiku0314,
+  },
+  {
+    id: 313,
+    name: KYOIKU_0313,
+    position: kyoiku0313,
+  },
+  {
+    id: 1306,
+    name: KYOIKU_1306,
+    position: kyoiku1306,
+  },
+  {
+    id: 1310,
+    name: KYOIKU_1310,
+    position: kyoiku1310,
+  },
+  {
+    id: 1106,
+    name: KYOIKU_1106,
+    position: kyoiku1106,
+  },
+  {
+    id: 1303,
+    name: KYOIKU_1303,
+    position: kyoiku1303,
+  },
+  {
+    id: 1105,
+    name: KYOIKU_1105,
+    position: kyoiku1105,
+  },
+  {
+    id: 1104,
+    name: KYOIKU_1104,
+    position: kyoiku1104,
+  },
+  {
+    id: 1305,
+    name: KYOIKU_1305,
+    position: kyoiku1305,
+  },
+  {
+    id: 1304,
+    name: KYOIKU_1304,
+    position: kyoiku1304,
+  },
+  {
+    id: 3203,
+    name: KYOIKU_3203,
+    position: kyoiku3203,
+  },
+  {
+    id: 1302,
+    name: KYOIKU_1302,
+    position: kyoiku1302,
+  },
+  {
+    id: 1102,
+    name: KYOIKU_1102,
+    position: kyoiku1102,
+  },
+  {
+    id: 1107,
+    name: KYOIKU_1107,
+    position: kyoiku1107,
+  },
+  {
+    id: 1108,
+    name: KYOIKU_1108,
+    position: kyoiku1108,
+  },
+  {
+    id: 1109,
+    name: KYOIKU_1109,
+    position: kyoiku1109,
+  },
+  {
+    id: 1110,
+    name: KYOIKU_1110,
+    position: kyoiku1110,
+  },
+  {
+    id: 1111,
+    name: KYOIKU_1111,
+    position: kyoiku1111,
+  },
+  {
+    id: 1205,
+    name: KYOIKU_1205,
+    position: kyoiku1205,
+  },
+  {
+    id: 1206,
+    name: KYOIKU_1206,
+    position: kyoiku1206,
+  },
+  {
+    id: 1207,
+    name: KYOIKU_1207,
+    position: kyoiku1207,
+  },
+  {
+    id: 1208,
+    name: KYOIKU_1208,
+    position: kyoiku1208,
+  },
+  {
+    id: 1209,
+    name: KYOIKU_1209,
+    position: kyoiku1209,
+  },
+  {
+    id: 1313,
+    name: KYOIKU_1313,
+    position: kyoiku1313,
+  },
+  {
+    id: 3101,
+    name: KYOIKU_3101,
+    position: kyoiku3101,
+  },
+  {
+    id: 3102,
+    name: KYOIKU_3102,
+    position: kyoiku3102,
+  },
+  {
+    id: 3103,
+    name: KYOIKU_3103,
+    position: kyoiku3103,
+  },
+  {
+    id: 3104,
+    name: KYOIKU_3104,
+    position: kyoiku3104,
+  },
+  {
+    id: 3201,
+    name: KYOIKU_3201,
+    position: kyoiku3201,
+  },
+];
+
+// 電気・電子系
+export const kyoikuElectricGroup: GuideProps[] = [
+  {
+    id: 4202,
+    name: KYOIKU_4202,
+    position: kyoiku4202,
+  },
+  {
+    id: 4305,
+    name: KYOIKU_4305,
+    position: kyoiku4305,
+  },
+  {
+    id: 4303,
+    name: KYOIKU_4303,
+    position: kyoiku4303,
+  },
+  {
+    id: 4203,
+    name: KYOIKU_4203,
+    position: kyoiku4203,
+  },
+  {
+    id: 4204,
+    name: KYOIKU_4204,
+    position: kyoiku4204,
+  },
+  {
+    id: 4302,
+    name: KYOIKU_4302,
+    position: kyoiku4302,
+  },
+  {
+    id: 5102,
+    name: KYOIKU_5102,
+    position: kyoiku5102,
+  },
+  {
+    id: 4306,
+    name: KYOIKU_4306,
+    position: kyoiku4306,
+  },
+  {
+    id: 4206,
+    name: KYOIKU_4206,
+    position: kyoiku4206,
+  },
+  {
+    id: 4205,
+    name: KYOIKU_4205,
+    position: kyoiku4205,
+  },
+  {
+    id: 4101,
+    name: KYOIKU_4101,
+    position: kyoiku4101,
+  },
+  {
+    id: 4102,
+    name: KYOIKU_4102,
+    position: kyoiku4102,
+  },
+  {
+    id: 4105,
+    name: KYOIKU_4105,
+    position: kyoiku4105,
+  },
+  {
+    id: 4106,
+    name: KYOIKU_4106,
+    position: kyoiku4106,
+  },
+  {
+    id: 4201,
+    name: KYOIKU_4201,
+    position: kyoiku4201,
+  },
+  {
+    id: 4207,
+    name: KYOIKU_4207,
+    position: kyoiku4207,
+  },
+  {
+    id: 4308,
+    name: KYOIKU_4308,
+    position: kyoiku4308,
+  },
+  {
+    id: 4406,
+    name: KYOIKU_4406,
+    position: kyoiku4406,
+  },
+];
+
+// 情報・ソフトウェア系
+export const kyoikuInformationTechnologyGroup: GuideProps[] = [
+  {
+    id: 1309,
+    name: KYOIKU_1309,
+    position: kyoiku1309,
+  },
+  {
+    id: 1307,
+    name: KYOIKU_1307,
+    position: kyoiku1307,
+  },
+  {
+    id: 7201,
+    name: KYOIKU_7201,
+    position: kyoiku7201,
+  },
+  {
+    id: 1204,
+    name: KYOIKU_1204,
+    position: kyoiku1204,
+  },
+  {
+    id: 4307,
+    name: KYOIKU_4307,
+    position: kyoiku4307,
+  },
+  {
+    id: 1313,
+    name: KYOIKU_1313,
+    position: kyoiku1313,
+  },
+  {
+    id: 1308,
+    name: KYOIKU_1308,
+    position: kyoiku1308,
+  },
+  {
+    id: 7101,
+    name: KYOIKU_7101,
+    position: kyoiku7101,
+  },
+  {
+    id: 1203,
+    name: KYOIKU_1203,
+    position: kyoiku1203,
+  },
+  {
+    id: 1311,
+    name: KYOIKU_1311,
+    position: kyoiku1311,
+  },
+  {
+    id: 115,
+    name: KYOIKU_0115,
+    position: kyoiku0115,
+  },
+  {
+    id: 4304,
+    name: KYOIKU_4304,
+    position: kyoiku4304,
+  },
+  {
+    id: 1202,
+    name: KYOIKU_1202,
+    position: kyoiku1202,
+  },
+  {
+    id: 1312,
+    name: KYOIKU_1312,
+    position: kyoiku1312,
+  },
+  {
+    id: 5207,
+    name: KYOIKU_5207,
+    position: kyoiku5207,
+  },
+];
+
+// 化学・バイオ系
+export const kyoikuChemistryGroup: GuideProps[] = [
+  {
+    id: 2301,
+    name: KYOIKU_2301,
+    position: kyoiku2301,
+  },
+  {
+    id: 2406,
+    name: KYOIKU_2406,
+    position: kyoiku2406,
+  },
+  {
+    id: 115,
+    name: KYOIKU_0115,
+    position: kyoiku0115,
+  },
+  {
+    id: 2106,
+    name: KYOIKU_2106,
+    position: kyoiku2106,
+  },
+  {
+    id: 2203,
+    name: KYOIKU_2203,
+    position: kyoiku2203,
+  },
+  {
+    id: 2103,
+    name: KYOIKU_2103,
+    position: kyoiku2103,
+  },
+  {
+    id: 2304,
+    name: KYOIKU_2304,
+    position: kyoiku2304,
+  },
+  {
+    id: 2403,
+    name: KYOIKU_2403,
+    position: kyoiku2403,
+  },
+  {
+    id: 6101,
+    name: KYOIKU_6101,
+    position: kyoiku6101,
+  },
+  {
+    id: 2206,
+    name: KYOIKU_2206,
+    position: kyoiku2206,
+  },
+  {
+    id: 2401,
+    name: KYOIKU_2401,
+    position: kyoiku2401,
+  },
+  {
+    id: 6201,
+    name: KYOIKU_6201,
+    position: kyoiku6201,
+  },
+  {
+    id: 2101,
+    name: KYOIKU_2101,
+    position: kyoiku2101,
+  },
+  {
+    id: 2104,
+    name: KYOIKU_2104,
+    position: kyoiku2104,
+  },
+  {
+    id: 2204,
+    name: KYOIKU_2204,
+    position: kyoiku2204,
+  },
+  {
+    id: 2208,
+    name: KYOIKU_2208,
+    position: kyoiku2208,
+  },
+  {
+    id: 2305,
+    name: KYOIKU_2305,
+    position: kyoiku2305,
+  },
+  {
+    id: 2307,
+    name: KYOIKU_2307,
+    position: kyoiku2307,
+  },
+  {
+    id: 2308,
+    name: KYOIKU_2308,
+    position: kyoiku2308,
+  },
+  {
+    id: 2402,
+    name: KYOIKU_2402,
+    position: kyoiku2402,
+  },
+  {
+    id: 2404,
+    name: KYOIKU_2404,
+    position: kyoiku2404,
+  },
+  {
+    id: 2407,
+    name: KYOIKU_2407,
+    position: kyoiku2407,
+  },
+  {
+    id: 5105,
+    name: KYOIKU_5105,
+    position: kyoiku5105,
+  },
+  {
+    id: 5106,
+    name: KYOIKU_5106,
+    position: kyoiku5106,
+  },
+  {
+    id: 6102,
+    name: KYOIKU_6102,
+    position: kyoiku6102,
+  },
+  {
+    id: 6103,
+    name: KYOIKU_6103,
+    position: kyoiku6103,
+  },
+  {
+    id: 6204,
+    name: KYOIKU_6204,
+    position: kyoiku6204,
+  },
+  {
+    id: 6203,
+    name: KYOIKU_6203,
+    position: kyoiku6203,
+  },
+  {
+    id: 6202,
+    name: KYOIKU_6202,
+    position: kyoiku6202,
+  },
+  {
+    id: 6205,
+    name: KYOIKU_6205,
+    position: kyoiku6205,
+  },
+  {
+    id: 2108,
+    name: KYOIKU_2108,
+    position: kyoiku2108,
+  },
+  {
+    id: 2408,
+    name: KYOIKU_2408,
+    position: kyoiku2408,
+  },
+];
+
+// 総合科学
+export const kyoikuIntegratedScience: GuideProps[] = [
+  {
+    id: 5204,
+    name: KYOIKU_5204,
+    position: kyoiku5204,
+  },
+  {
+    id: 5203,
+    name: KYOIKU_5203,
+    position: kyoiku5203,
+  },
+  {
+    id: 312,
+    name: KYOIKU_0312,
+    position: kyoiku0312,
+  },
+  {
+    id: 5206,
+    name: KYOIKU_5206,
+    position: kyoiku5206,
+  },
+  {
+    id: 5205,
+    name: KYOIKU_5205,
+    position: kyoiku5205,
+  },
+  {
+    id: 5201,
+    name: KYOIKU_5201,
+    position: kyoiku5201,
+  },
+  {
+    id: 303,
+    name: KYOIKU_0303,
+    position: kyoiku0303,
+  },
+  {
+    id: 310,
+    name: KYOIKU_0310,
+    position: kyoiku0310,
+  },
+  {
+    id: 306,
+    name: KYOIKU_0306,
+    position: kyoiku0306,
+  },
+  {
+    id: 5104,
+    name: KYOIKU_5104,
+    position: kyoiku5104,
+  },
+  {
+    id: 301,
+    name: KYOIKU_0301,
+    position: kyoiku0301,
+  },
+  {
+    id: 5103,
+    name: KYOIKU_5103,
+    position: kyoiku5103,
+  },
+  {
+    id: 302,
+    name: KYOIKU_0302,
+    position: kyoiku0302,
+  },
+  {
+    id: 305,
+    name: KYOIKU_0305,
+    position: kyoiku0305,
+  },
+  {
+    id: 5202,
+    name: KYOIKU_5202,
+    position: kyoiku5202,
+  },
+  {
+    id: 311,
+    name: KYOIKU_0311,
+    position: kyoiku0311,
+  },
+];
+
+// 管理・教育棟
+// 1号棟
+// 2号棟
+// 3号棟
+// 4号棟
+// 5号棟
+// 6号棟
+// 7号棟
+
+// 管理・教育棟＆1~7号棟全て
+export const kyoikuDivideAll: GuideProps[] = [
   {
     id: 101,
     name: KYOIKU_0101,
