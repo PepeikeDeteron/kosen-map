7b33c66bf113a736a039b90ca98b7d10c1cdc66f
Yoshiaki SASAKI <pepeikedeteron@gmail.com>
2022-01-21 16:52:30 +0900

    Storybook を追加

---
+++ src/components/molecules/EntranceButton/index.stories.tsx
@@ -0,0 +1,23 @@
+import React, { ComponentProps } from 'react';
+import { Meta, Story } from '@storybook/react';
+import EntranceButton from '.';
+
+export default {
+  title: 'components/molecules/EntranceButton',
+  components: EntranceButton,
+  argTypes: {
+    onClick: {
+      action: 'clicked',
+    },
+  },
+} as Meta;
+
+const Template: Story<ComponentProps<typeof EntranceButton>> = (props) => (
+  <EntranceButton {...props} />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  color: 'warning',
+  label: '正面玄関',
+};
