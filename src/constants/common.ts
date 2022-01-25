405540f12251eba0a5716dccc1c8be63f590f088
Yoshiaki SASAKI <pepeikedeteron@gmail.com>
2022-01-26 07:14:41 +0900

    1階の階段を追加

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -6,8 +6,15 @@ export const guideBoxProperties = {
   opacity: 0.65,
   transparent: true,
 };
+
 export const facilityBoxProperties = {
   color: 0xffff00,
+  opacity: 0.5,
+  transparent: true,
+};
+
+export const stairsBoxProperties = {
+  color: 0xffa000,
   opacity: 0.5,
   transparent: true,
 };
