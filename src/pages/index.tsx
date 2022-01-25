3af88dbfec4e91a2554f4bb28e4b034f406eb520
Yoshiaki SASAKI <pepeikedeteron@gmail.com>
2022-01-26 08:27:11 +0900

    Add: フッターを追加 (#162)
    * フッターコンポーネントを追加
    
    * フッターを表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { VFC } from 'react';
 import Head from 'next/head';
+import Footer from '@/components/organisms/Footer';
 import Template from '@/components/templates/Home';
 
 const Home: VFC = () => {
@@ -9,8 +10,8 @@ const Home: VFC = () => {
         <title>一関高専Map｜校舎案内</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Template />
+      <Footer />
     </>
   );
 };
